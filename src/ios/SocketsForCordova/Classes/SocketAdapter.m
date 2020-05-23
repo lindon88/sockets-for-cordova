fe941fdb14b730757e22790b75ea694919a24725
Lindon Camaj <lindoncamaj88@gmail.com>
2020-05-23 14:19:08 +0200

    removed unused files

--- src/ios/SocketsForCordova/Classes/SocketAdapter.m
+++ src/ios/SocketsForCordova/Classes/SocketAdapter.m
@@ -1,24 +1,6 @@
-/**
- * Copyright (c) 2015, Blocshop s.r.o.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the Blocshop s.r.o.. The name of the
- * Blocshop s.r.o. may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- */
-
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-#include <math.h>
 #import "SocketAdapter.h"
 
 CFReadStreamRef readStream;
@@ -28,8 +10,6 @@ NSInputStream *inputStream;
 NSOutputStream *outputStream;
 
 BOOL wasOpenned = FALSE;
-
-int const WRITE_BUFFER_SIZE = 10 * 1024;
 
 @implementation SocketAdapter
 
@@ -189,26 +169,13 @@ int const WRITE_BUFFER_SIZE = 10 * 1024;
 }
 
 - (void)write:(NSArray *)dataArray {
-    int numberOfBatches = ceil((float)dataArray.count / (float)WRITE_BUFFER_SIZE);
-    for (int i = 0; i < (numberOfBatches - 1); i++) {
-        [self writeSubarray:dataArray offset:i * WRITE_BUFFER_SIZE length:WRITE_BUFFER_SIZE];
+    uint8_t buf[dataArray.count];
+    for (int i = 0; i < dataArray.count; i++) {
+        buf[i] = (unsigned char)[[dataArray objectAtIndex:i] integerValue];
     }
-    int lastBatchPosition = (numberOfBatches - 1) * WRITE_BUFFER_SIZE;
-    [self writeSubarray:dataArray offset:lastBatchPosition length:(dataArray.count - lastBatchPosition)];
-}
-
-- (void)writeSubarray:(NSArray *)dataArray offset:(long)offset length:(long)length {
-    uint8_t buf[length];
-    for (long i = 0; i < length; i++) {
-        unsigned char byte = (unsigned char)[[dataArray objectAtIndex:(offset + i)] integerValue];
-        buf[i] = byte;
-    }
-    NSInteger bytesWritten = [outputStream write:buf maxLength:length];
+    NSInteger bytesWritten = [outputStream write:buf maxLength:dataArray.count];
     if (bytesWritten == -1) {
         @throw [NSException exceptionWithName:@"SocketException" reason:[outputStream.streamError localizedDescription] userInfo:nil];
-    }
-    if (bytesWritten != length) {
-        [self writeSubarray:dataArray offset:(offset + bytesWritten) length:(length - bytesWritten)];
     }
 }
 
